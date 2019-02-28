7198d6f371c5bfd1f23a794a2078a819acc1fa55
Miguel Chan <vvchan@outlook.com>
2019-02-28 17:45:31 +0800

    ..

--- graphics/assignments/test/src/main.cpp
+++ graphics/assignments/test/src/main.cpp
@@ -10,8 +10,7 @@
 
 using namespace std;
 
-void displayMe(void)
-{
+void displayMe(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
     glVertex3f(0.0, 0.0, 0.0);
@@ -23,22 +22,47 @@ void displayMe(void)
 
 }
 
-int main(int argc, char** argv)
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-//    glutInit(&argc, argv);
-//    glutInitDisplayMode(GLUT_SINGLE);
-//    glutInitWindowSize(300, 300);
-//    glutInitWindowPosition(100, 100);
-//    glutCreateWindow("Hello world :D");
-//    glutDisplayFunc(displayMe);
-//    glutMainLoop();
-//    cout << glGetString(GL_VERSION) << endl;
-//    printf("%s\n", glGetString(GL_VERSION));
+    glViewport(0, 0, width, height);
+}
+
+int main(int argc, char **argv) {
 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#if defined(__APPLE__)
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
 
     return 0;
 }
