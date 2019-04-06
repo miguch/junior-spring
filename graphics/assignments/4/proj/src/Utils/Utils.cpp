72b17cda738d0527bcd58cec0c151c61bfe6c1e8
Miguel Chan <vvchan@outlook.com>
2019-04-06 17:06:02 +0800

    Update

---
+++ graphics/assignments/4/proj/src/Utils/Utils.cpp
@@ -0,0 +1,100 @@
+//
+// Created by Miguel Chan on 2019-04-02.
+//
+
+#include "Utils.h"
+#include <glad/glad.h>
+#include <functional>
+#include <iostream>
+#include <shared_mutex>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+using namespace std;
+
+namespace Utils {
+
+    void enableDepthTest() {
+        glEnable(GL_DEPTH_TEST);
+    }
+
+    void disableDepthTest() {
+        glDisable(GL_DEPTH_TEST);
+    }
+
+    struct ScreenSize {
+        GLuint screenWidth, screenHeight;
+        mutable shared_mutex mutex;
+    } screenSize;
+    std::pair<GLuint, GLuint> getScreenSize() {
+        shared_lock<shared_mutex> guard(screenSize.mutex);
+        return {screenSize.screenWidth, screenSize.screenHeight};
+    }
+
+    void setScreenSize(GLuint w, GLuint h) {
+        unique_lock<shared_mutex> guard(screenSize.mutex);
+        screenSize.screenWidth = w;
+        screenSize.screenHeight = h;
+    }
+
+    void printMat4(const glm::mat4& mat) {
+        for (int i = 0; i < 4; i++) {
+            for (int k = 0; k < 4; k++) {
+                cout << mat[k][i] << ' ';
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+
+    GLToolsException::GLToolsException(const char *msg)  : msg(msg) {}
+
+    const char *GLToolsException::what() const noexcept {
+        return msg.c_str();
+    }
+
+    auto vertexAttribSetter = []() {
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
+        glEnableVertexAttribArray(0);
+        //Color
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+    };
+
+    void VertexBuffer::bind() {
+        glBindVertexArray(VAO);
+    }
+
+    VertexBuffer::~VertexBuffer() {
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+    }
+
+    VertexBuffer::VertexBuffer(int size, float *data, const std::function<void()> &vertexAttribCallback) {
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        if (size != 0 && data != nullptr) {
+            glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+        }
+        vertexAttribCallback();
+        // Unbind
+        glBindVertexArray(0);
+    }
+
+    ElementBuffer::~ElementBuffer() {
+        glDeleteBuffers(1, &EBO);
+    }
+
+    ElementBuffer::ElementBuffer(GLuint VAO, int size, unsigned int *data) {
+        glBindVertexArray(VAO);
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        if (size != 0 && data != nullptr) {
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+        }
+        glBindVertexArray(0);
+    }
+}
--- graphics/assignments/template/src/main.cpp
+++ graphics/assignments/template/src/main.cpp
@@ -1,221 +1,22 @@
 #include <iostream>
-#include <memory>
-#include <cmath>
-#include <ctime>
-#include <thread>
-#include <exception>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <imgui/imgui.h>
-#include <imgui/imgui_impl_glfw.h>
-#include <imgui/imgui_impl_opengl3.h>
+
 #include <glm/glm.hpp>
+#include <vector>
+#include <unordered_map>
 
-#include "shader.h"
-
-#include "stb_image.h"
+#include "MainLoop.h"
 
 using namespace std;
 
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-const char* glsl_version;
+const unsigned int SCR_WIDTH = 600;
+const unsigned int SCR_HEIGHT = 400;
 
-pair<GLuint, GLuint> genVertex(int size, float *data,
-                               const function<void()> &vertexAttribCallback) {
-    GLuint VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    if (size != 0 && data != nullptr) {
-        glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
-    }
-    vertexAttribCallback();
-    // Unbind
-    glBindVertexArray(0);
-    return make_pair(VAO, VBO);
-}
-
-GLuint genEBO(GLuint VAO, int size, unsigned int *data) {
-    GLuint EBO;
-    glBindVertexArray(VAO);
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
-    glBindVertexArray(0);
-    return EBO;
-}
-
-void gui(GLFWwindow* window) {
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
-
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-// Setup Platform/Renderer bindings
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.f, 0.f, 0.f, 1.00f);
-
-    // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
-        this_thread::sleep_for(chrono::milliseconds(16));
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        glfwPollEvents();
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-            clear_color.x += 0.1 * f;
-            clear_color.y += 0.2 * f;
-            clear_color.z += 0.5 * f;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwMakeContextCurrent(window);
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-
-        glfwMakeContextCurrent(window);
-        glfwSwapBuffers(window);
-    }
-
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-}
-
-void triangle(GLFWwindow* window) {
-    float vertices[] = {
-            1.f, -1.f, 0.0f,  1.0f, 0.0f, 0.0f,
-            -1.f, -1.f, 0.0f,  0.0f, 1.0f, 0.0f,
-            0.0f,  1.f, 0.0f,  0.0f, 0.0f, 1.0f,
-            1.0f,  1.f, 0.0f,  0.0f, 1.0f, 1.0f,
-            -1.0f, 1.f, 0.0f, 1.0f, 1.f, 0.f,
-            -0.5f, 0.5f, 0.0f, 1.0f, 0.f, 1.f
-    };
-
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-
-    GLuint VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    unique_ptr<Shader> shader;
-    try {
-        shader = std::make_unique<Shader>("shaders/vert.glsl", "shaders/frag.glsl");
-    } catch (exception &err) {
-        cerr << err.what() << endl;
-        exit(1);
-    }
-
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    //Position
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    //Color
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.f, 0.f, 0.f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        shader->use();
-
-        this_thread::sleep_for(chrono::milliseconds(46));
-
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glfwMakeContextCurrent(window);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-}
 
 int main() {
     glfwInit();
 
-    // GL 3.2 + GLSL 150
-    glsl_version = "#version 150";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
@@ -225,6 +26,9 @@ int main() {
 
     // glfw window creation
     GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello", NULL, NULL);
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int w, int h) {
+        glViewport(0, 0, w, h);
+    });
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -238,10 +42,14 @@ int main() {
         return -1;
     }
 
+    try {
+        MainLoop::loop(window);
+    } catch (exception &err) {
+        cerr << err.what() << endl;
+        exit(1);
+    }
 
-    triangle(window);
-
-
+    glfwDestroyWindow(window);
     // clear GLFW resources
     glfwTerminate();
     return 0;
